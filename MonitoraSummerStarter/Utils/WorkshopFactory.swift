4c1fb8780829a2e186a93c6cc5d6186803443ee0
yabenatti <min.benatti@gmail.com>
2018-02-17 16:18:31 -0200

    Creates basic files

---
+++ MonitoraSummerStarter/Utils/WorkshopFactory.swift
@@ -0,0 +1,9 @@
+//
+//  WorkshopFactory.swift
+//  MonitoraSummerStarter
+//
+//  Created by Yasmin Benatti on 2018-02-17.
+//  Copyright © 2018 Yasmin Benatti. All rights reserved.
+//
+
+import Foundation
